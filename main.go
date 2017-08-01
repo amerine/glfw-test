e80657d07cb60d8e3907ad3307fddd7efccc5d7a
Mark Turner <mark@amerine.net>
2017-08-01 13:00:41 -0700

    maybe log then quit?

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"runtime"
 
 	"github.com/go-gl/glfw/v3.2/glfw"
@@ -13,13 +14,13 @@ func init() {
 func main() {
 	err := glfw.Init()
 	if err != nil {
-		panic(err)
+		log.Fatalln("failed to initialize glfw:", err)
 	}
 	defer glfw.Terminate()
 
 	window, err := glfw.CreateWindow(400, 400, "glfw testing", nil, nil)
 	if err != nil {
-		panic(err)
+		log.Fatalln("failed to create glfw window:", err)
 	}
 
 	window.SetSizeLimits(400, 400, 400, 400)
